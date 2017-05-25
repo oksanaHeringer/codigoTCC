534e351ae2393b49791ec4071c7e6bc5dc25c467
Oksana Heringer da Silva <oksanaheringer@gmail.com>
2017-05-25 19:05:02 -0300

    Atualizados

---
+++ reg.jl
@@ -0,0 +1,50 @@
+using Krylov
+using ForwardDiff
+using LinearOperators
+
+function reg_conf(nlp; η = 0.25 ,η1 = 0.25, η2 = 0.75, σ1 = 0.5, σ2 = 2.0, atol = 1e-6, rtol = 1e-4)
+    k_max = 10000
+    tempo_max = 30
+    saida = 0
+    tempo = 0.0
+    tempo_inicial = time()
+    f(x) = obj(nlp, x)
+    g(x) = grad(nlp, x)
+    H(x) = hess_op(nlp, x)
+    x = nlp.meta.x0
+    fx = f(x)
+    gx = g(x)
+    Hx = H(x)
+    n = nlp.meta.nvar
+    Δ = min(max(0.1*norm(gx), 1), 100)
+    tol = atol + rtol*norm(gx)
+    k = 0
+    while norm(gx) > tol
+        d, stats = cg(Hx,-gx, radius=Δ)
+        ared = fx - f(x + d)
+        pred = -dot(d,gx) - 0.5*dot(Hx*d,d)
+        ρ = ared/pred
+        if ρ > η
+            x = x + d
+        end
+        if ρ < η1
+            Δ = σ1*Δ
+        elseif ρ > η2
+            Δ = σ2*Δ
+        end
+        if k >= k_max
+            saida = 1
+            break
+        end
+        tempo = time() - tempo_inicial
+        if tempo >= tempo_max
+            saida = 2
+            break
+        end
+        gx = g(x)
+        fx = f(x)
+        Hx = H(x)
+        k = k + 1
+    end
+    return x, fx, norm(gx)
+end
---
+++ teste_penalidade.jl
@@ -0,0 +1,56 @@
+using Base.Test
+using NLPModels
+using ForwardDiff
+
+include("reg.jl")
+
+function penalidade_quadratica(nlp;μ=1.0, ϵ=1e-6, max_iter=1000, max_time=30)
+    exit_flag = 0
+    iter = 1
+    x = nlp.meta.x0
+    f(x) = obj(nlp, x)
+    c(x) = cons(nlp, x)
+    gx = grad(nlp, x)
+    Jx = jac_op(nlp, x)
+    λ = cgls(Jx', -gx)[1]
+    ∇L = gx + Jx'*λ
+    fx = f(x)
+    cx = c(x)
+    ϵsub = 1.0
+
+    start_time = time()
+    elapsed_time = 0.0
+
+    while norm(∇L) > ϵ || norm(cx) > ϵ && (iter < max_iter)
+      subnlp = create_sub_problem(nlp, x, μ)
+      x, fx, ng = reg_conf(subnlp)
+      μ = 10*μ
+      ϵsub = 0.1*ϵsub
+      fx = f(x)
+      cx = c(x)
+      gx = grad(nlp, x)
+      Jx = jac_op(nlp, x)
+      λ = cgls(Jx', -gx)[1]
+      ∇L = gx + Jx'*λ
+
+      iter = iter + 1
+      if iter >= max_iter
+          exit_flag = 1
+          break
+      end
+      elapsed_time = time() - start_time
+      if elapsed_time >= max_time
+          exit_flag = 2
+          break
+      end
+    end
+return x, fx, norm(∇L), norm(cx), iter, elapsed_time, exit_flag
+end
+
+function create_sub_problem(nlp, x, μ)
+  c(x) = cons(nlp, x)
+  Q(x) = obj(nlp, x) + μ/2*norm(c(x))^2
+  ∇Q(x) = grad(nlp, x) + μ*jtprod(nlp, x, c(x))
+  HQv(x, v; obj_weight=1.0) = hprod(nlp, x, v, y=μ*c(x)) + μ*jtprod(nlp, x, jprod(nlp, x, v))
+  subnlp = SimpleNLPModel(Q, x, g=∇Q, Hp=HQv)
+end
